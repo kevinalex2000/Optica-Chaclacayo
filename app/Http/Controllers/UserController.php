77c16cbec12a7ecf6499d5ede694f46ad5e46cbe
LAPTOP-JCTSA2E5\AXZ <goviedo@pecano.pe>
2021-04-25 01:54:04 -0500

    Module Users Complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Rol;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -25,7 +26,8 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('user.create');
+        $rols = Rol::all();
+        return view('user.create')->with('rols', $rols);;
     }
 
     /**
@@ -36,7 +38,86 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $emailExist = User::where('email', $request->post('email'))->count();
+        if($emailExist > 0){
+            $messageResult = array(
+                "type" => "warning",
+                "message" => "<i class='fas fa-exclamation-triangle mr-2'></i> No se ha podido crear el usuario debido que el email <b>".$request->post('email')."</b> ya esta siendo utilizado"
+            );
+            return redirect("/users/create")->with('messageResult', $messageResult);
+        }
+        $usernameExist = User::where('username', $request->post('username'))->count();
+        if($usernameExist > 0){
+            $messageResult = array(
+                "type" => "warning",
+                "message" => "<i class='fas fa-exclamation-triangle mr-2'></i> No se ha podido crear el usuario debido que el nombre de usuario <b>".$request->post('username')."</b> ya esta siendo utilizado"
+            );
+            return redirect("/users/create")->with('messageResult', $messageResult);
+        }
+
+        $userExist = User::where('dni', $request->post('dni'))->count();
+        $password = $request->post('password');
+        $password_repeat = $request->post('password_repeat');
+        // Si el usere con dni no existe o si es el mismo
+        if($userExist <= 1){
+            if($password != ""){
+                if($password == $password_repeat){
+                    $user = new User();
+
+                    $user->dni = $request->post('dni');
+                    $user->name = $request->post('name');
+                    $user->lastname = $request->post('lastname');
+                    $user->phone = $request->post('phone');
+                    $user->direction = $request->post('direction');
+                    $user->email = $request->post('email');
+                    $user->username = $request->post('username');
+                    $user->password = $request->post('password');
+                    $user->id_rol = $request->post('id_rol');
+
+                    $user->save();
+
+                    $messageResult = array(
+                        "type" => "success",
+                        "message" => "<i class='fas fa-check mr-2'></i> El Usuario <b>".$request->post('name')."</b> ha sido creado exitosamente"
+                    );
+                }
+                else{
+                    $messageResult = array(
+                        "type" => "warning",
+                        "message" => "<i class='fas fa-exclamation-triangle mr-2'></i> Las contraseñas no coinciden"
+                    );
+                }
+
+            }
+            else{
+                $user = new User();
+
+                $user->dni = $request->post('dni');
+                $user->name = $request->post('name');
+                $user->lastname = $request->post('lastname');
+                $user->phone = $request->post('phone');
+                $user->direction = $request->post('direction');
+                $user->email = $request->post('email');
+                $user->username = $request->post('username');
+                $user->id_rol = $request->post('id_rol');
+
+                $user->save();
+
+                $messageResult = array(
+                    "type" => "success",
+                    "message" => "<i class='fas fa-check mr-2'></i> El Usuario <b>".$request->post('name')."</b> ha sido creado exitosamente"
+                );
+            }
+
+
+        }else{
+            $messageResult = array(
+                "type" => "warning",
+                "message" => "<i class='fas fa-exclamation-triangle mr-2'></i> No se ha podido editar el usuario debido que el DNI <b>".$request->post('dni')."</b> ya esta siendo utilizado"
+            );
+        }
+
+        return redirect("/users/create")->with('messageResult', $messageResult);
     }
 
     /**
@@ -59,7 +140,8 @@ class UserController extends Controller
     public function edit($id)
     {
         $user = User::find($id);
-        return view('user.edit')->with('user', $user);
+        $rols = Rol::all();
+        return view('user.edit')->with('data',array("User" =>$user, "Rols" => $rols));
     }
 
     /**
@@ -71,7 +153,85 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $emailExist = User::where('email', $request->post('email'))->count();
+        if($emailExist > 0){
+            $messageResult = array(
+                "type" => "warning",
+                "message" => "<i class='fas fa-exclamation-triangle mr-2'></i> No se ha podido editar el usuario debido que el email <b>".$request->post('email')."</b> ya esta siendo utilizado"
+            );
+            return redirect("/users/".$id."/edit")->with('messageResult', $messageResult);
+        }
+        $usernameExist = User::where('username', $request->post('username'))->count();
+        if($usernameExist > 0){
+            $messageResult = array(
+                "type" => "warning",
+                "message" => "<i class='fas fa-exclamation-triangle mr-2'></i> No se ha podido editar el usuario debido que el nombre de usuario <b>".$request->post('username')."</b> ya esta siendo utilizado"
+            );
+            return redirect("/users/".$id."/edit")->with('messageResult', $messageResult);
+        }
+        $userExist = User::where('dni', $request->post('dni'))->count();
+        $password = $request->post('password');
+        $password_repeat = $request->post('password_repeat');
+        // Si el usere con dni no existe o si es el mismo
+        if($userExist <= 1){
+            if($password != ""){
+                if($password == $password_repeat){
+                    $user = User::find($id);
+
+                    $user->dni = $request->post('dni');
+                    $user->name = $request->post('name');
+                    $user->lastname = $request->post('lastname');
+                    $user->phone = $request->post('phone');
+                    $user->direction = $request->post('direction');
+                    $user->email = $request->post('email');
+                    $user->username = $request->post('username');
+                    $user->password = $request->post('password');
+                    $user->id_rol = $request->post('id_rol');
+
+                    $user->save();
+
+                    $messageResult = array(
+                        "type" => "success",
+                        "message" => "<i class='fas fa-check mr-2'></i> El Usuario <b>".$request->post('name')."</b> ha sido editado exitosamente"
+                    );
+                }
+                else{
+                    $messageResult = array(
+                        "type" => "warning",
+                        "message" => "<i class='fas fa-exclamation-triangle mr-2'></i> Las contraseñas no coinciden"
+                    );
+                }
+
+            }
+            else{
+                $user = User::find($id);
+
+                $user->dni = $request->post('dni');
+                $user->name = $request->post('name');
+                $user->lastname = $request->post('lastname');
+                $user->phone = $request->post('phone');
+                $user->direction = $request->post('direction');
+                $user->email = $request->post('email');
+                $user->username = $request->post('username');
+                $user->id_rol = $request->post('id_rol');
+
+                $user->save();
+
+                $messageResult = array(
+                    "type" => "success",
+                    "message" => "<i class='fas fa-check mr-2'></i> El Usuario <b>".$request->post('name')."</b> ha sido editado exitosamente"
+                );
+            }
+
+
+        }else{
+            $messageResult = array(
+                "type" => "warning",
+                "message" => "<i class='fas fa-exclamation-triangle mr-2'></i> No se ha podido editar el usuario debido que el DNI <b>".$request->post('dni')."</b> ya esta siendo utilizado"
+            );
+        }
+
+        return redirect("/users/".$id."/edit")->with('messageResult', $messageResult);
     }
 
     /**
@@ -82,6 +242,14 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+
+        $messageResult = array(
+            "type" => "danger",
+            "message" => "<i class='fas fa-trash mr-2'></i> El usuario <b>".$user->name."</b> ha sido eliminado exitosamente"
+        );
+
+        return redirect("/users")->with('messageResult', $messageResult);
     }
 }
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -21,10 +21,16 @@
 
     </ul>
 </div>
-
+@if(session('messageResult'))
+<div class="container-fluid">
+    <div class="alert alert-{{session('messageResult')['type']}}">{!!trans(session('messageResult')["message"])!!}</div>
+</div>
+@endif
 <!-- Content -->
 <div class="container-fluid">
-    <form action="{{route('users.update', $user->id)}}" class="form-neon" autocomplete="off">
+    <form action="{{route('users.update', $data["User"]->id)}}" method="post" class="form-neon" autocomplete="off">
+        @csrf
+        @method('put')
         <fieldset>
             <legend><i class="far fa-address-card"></i> &nbsp; Información personal</legend>
             <div class="container-fluid">
@@ -32,32 +38,32 @@
                     <div class="col-12 col-md-4">
                         <div class="form-group">
                             <label for="usuario_dni" class="bmd-label-floating">DNI</label>
-                            <input type="text" pattern="[0-9-]{1,20}" class="form-control" name="usuario_dni_up" id="usuario_dni" value="{{$user->dni}}" maxlength="20">
+                            <input type="text" pattern="[0-9-]{1,20}" class="form-control" name="dni" id="usuario_dni" value="{{$data["User"]->dni}}" maxlength="20" required>
                         </div>
                     </div>
 
                     <div class="col-12 col-md-4">
                         <div class="form-group">
                             <label for="usuario_nombre" class="bmd-label-floating">Nombres</label>
-                            <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_nombre_up" value="{{$user->name}}" id="usuario_nombre" maxlength="35">
+                            <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="name" value="{{$data["User"]->name}}" id="usuario_nombre" maxlength="35" required>
                         </div>
                     </div>
                     <div class="col-12 col-md-4">
                         <div class="form-group">
                             <label for="usuario_apellido" class="bmd-label-floating">Apellidos</label>
-                            <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_apellido_up" value="{{$user->lastname}}" id="usuario_apellido" maxlength="35">
+                            <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="lastname" value="{{$data["User"]->lastname}}" id="usuario_apellido" maxlength="35">
                         </div>
                     </div>
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_telefono" class="bmd-label-floating">Teléfono</label>
-                            <input type="text" pattern="[0-9()+]{8,20}" class="form-control" name="usuario_telefono_up" id="usuario_telefono" value="{{$user->phone}}" maxlength="20">
+                            <input type="text" pattern="[0-9()+]{8,20}" class="form-control" name="usuario_telefono_up" id="phone" value="{{$data["User"]->phone}}" maxlength="20">
                         </div>
                     </div>
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_direccion" class="bmd-label-floating">Dirección</label>
-                            <input type="text" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}" class="form-control" name="usuario_direccion_up" value="{{$user->direction}}" id="usuario_direccion" maxlength="190">
+                            <input type="text" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}" class="form-control" name="direction" value="{{$data["User"]->direction}}" id="usuario_direccion" maxlength="190">
                         </div>
                     </div>
                 </div>
@@ -68,16 +74,16 @@
             <legend><i class="fas fa-user-lock"></i> &nbsp; Información de la cuenta</legend>
             <div class="container-fluid">
                 <div class="row">
-                    <!--<div class="col-12 col-md-6">
+                    <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_usuario" class="bmd-label-floating">Nombre de usuario</label>
-                            <input type="text" pattern="[a-zA-Z0-9]{1,35}" class="form-control" name="usuario_usuario_up" id="usuario_usuario" maxlength="35">
+                            <input type="text" pattern="[a-zA-Z0-9]{1,35}" class="form-control" name="username" id="username" value="{{$data["User"]->username}}" maxlength="35" required>
                         </div>
-                    </div>-->
-                    <div class="col-12 col-md-12">
+                    </div>
+                    <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_email" class="bmd-label-floating">Email</label>
-                            <input type="email" class="form-control" name="usuario_email_up" id="usuario_email" value="{{$user->email}}" maxlength="70">
+                            <input type="email" class="form-control" name="email" id="email" value="{{$data["User"]->email}}" maxlength="70" required>
                         </div>
                     </div>
                     <!--<div class="col-12">
@@ -101,13 +107,13 @@
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_clave_nueva_1" class="bmd-label-floating">Contraseña</label>
-                            <input type="password" class="form-control" name="usuario_clave_nueva_1" id="usuario_clave_nueva_1" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" >
+                            <input type="password" class="form-control"  name="password" id="usuario_clave_nueva_1" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" >
                         </div>
                     </div>
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_clave_nueva_2" class="bmd-label-floating">Repetir contraseña</label>
-                            <input type="password" class="form-control" name="usuario_clave_nueva_2" id="usuario_clave_nueva_2" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" >
+                            <input type="password" class="form-control" name="password_repeat" id="usuario_clave_nueva_2" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" >
                         </div>
                     </div>
                 </div>
@@ -119,13 +125,19 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-                        <p><span class="badge badge-info">Administrador</span> Permisos para registrar, actualizar y eliminar</p>
-                        <p><span class="badge badge-success">Asistente de Ventas</span> Permisos para registrar y actualizar</p>
+                        @foreach ($data["Rols"] as $rol)
+                                <p><span class="badge {{$rol->leyend}}">{{$rol->name}}</span> {{$rol->description}}</p>
+                        @endforeach
                         <div class="form-group">
-                            <select class="form-control" name="usuario_privilegio_up">
+                            <select class="form-control" name="id_rol" required>
                                 <option value="" selected="" disabled="">Seleccione una opción</option>
-                                <option value="1">Administrador</option>
-                                <option value="2">Asistente de Ventas</option>
+                                @foreach ($data["Rols"] as $rol)
+                                @if($rol->id == $data["User"]->id_rol)
+                                <option selected value="{{$rol->id}}">{{$rol->name}}</option>
+                                @else
+                                <option value="{{$rol->id}}">{{$rol->name}}</option>
+                                @endif
+                                @endforeach
                             </select>
                         </div>
                     </div>
@@ -133,7 +145,7 @@
             </div>
         </fieldset>
         <br><br><br>
-        <fieldset>
+        <!--<fieldset>
             <p class="text-center">Para poder guardar los cambios en esta cuenta debe de ingresar su nombre de usuario y contraseña</p>
             <div class="container-fluid">
                 <div class="row">
@@ -151,16 +163,16 @@
                     </div>
                 </div>
             </div>
-        </fieldset>
+        </fieldset>-->
         <p class="text-center" style="margin-top: 40px;">
             <button type="submit" class="btn btn-raised btn-success btn-sm"><i class="fas fa-sync-alt"></i> &nbsp; ACTUALIZAR</button>
         </p>
     </form>
 
-    <div class="alert alert-danger text-center" role="alert">
+    <!--<div class="alert alert-danger text-center" role="alert">
         <p><i class="fas fa-exclamation-triangle fa-5x"></i></p>
         <h4 class="alert-heading">¡Ocurrió un error inesperado!</h4>
         <p class="mb-0">Lo sentimos, no podemos mostrar la información solicitada debido a un error.</p>
-    </div>
+    </div>-->
 </div>
 @endsection
