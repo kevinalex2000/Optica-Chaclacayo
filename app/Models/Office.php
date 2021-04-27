c34ada2202629b2661f001d016468c6fb907ec24
kevinalexdev <kevinalexpe@gmail.com>
2021-04-26 23:26:30 -0500

    tabla office

---
+++ app/Models/Office.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Office extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'address',
+        'rental_price',
+        'id_user',
+        'phone',
+    ];
+    
+    public function client(){
+        return $this->belongsTo(User::Class, "id_user");
+    }
+}
